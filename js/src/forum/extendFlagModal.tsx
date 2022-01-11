4647ad3a4c37aba73da839e5285d4efcf5eb5df6
Ian Morland <ian@morland.me>
2022-01-11 11:31:56 +0000

    Show dupe discussion title in flag border, preselect dupe discussion in merge modal

--- js/src/forum/extendFlagModal.tsx
+++ js/src/forum/extendFlagModal.tsx
@@ -2,9 +2,8 @@ import app from 'flarum/forum/app';
 import { extend, override } from 'flarum/common/extend';
 import FlagPostModal from 'flarum/flags/forum/components/FlagPostModal';
 import { findFirstVdomChild } from './utils/findVdomChild';
-import withAttr from 'flarum/utils/withAttr';
-import Stream from 'flarum/utils/Stream';
-import Post from 'flarum/components/Post';
+import withAttr from 'flarum/common/utils/withAttr';
+import Post from 'flarum/forum/components/Post';
 import Link from 'flarum/common/components/Link';
 import type Mithril from 'mithril';
 import icon from 'flarum/common/helpers/icon';
@@ -13,37 +12,42 @@ import GlobalSearchState from 'flarum/forum/states/GlobalSearchState';
 import Discussion from 'flarum/common/models/Discussion';
 import Flag from 'flarum/flags/forum/models/flag';
 import ItemList from 'flarum/common/utils/ItemList';
+import Button from 'flarum/common/components/Button';
+import Stream from 'flarum/common/utils/Stream';
+import LoadingIndicator from 'flarum/common/components/LoadingIndicator';
 
 export default function extendFlagModal() {
   // https://github.com/flarum/flags/pull/39 is not available
   if (!FlagPostModal.prototype.flagReasons) {
     extend(FlagPostModal.prototype, 'content', function (vnode: Mithril.Vnode) {
-        if (this.attrs.post?.number() === 1) {
-          findFirstVdomChild(vnode, '.Form-group', (vnode) => {
-            const discussion = this.attrs.post.discussion();
-            this.search = new GlobalSearchState();
-  
-            vnode.children[0].children.splice(3, 0,
-              <label className="checkbox">
-                <input type="radio" name="reason" checked={this.reason() === 'duplicate'} value="duplicate" onclick={withAttr('value', this.reason)} />
-                <strong>{app.translator.trans('blomstra-flag-duplicates.forum.flags.reason_duplicate_label')}</strong>
-                {app.translator.trans('blomstra-flag-duplicates.forum.flags.reason_duplicate_text')}
-                {this.reason() === 'duplicate' ? (
-                  <components.DiscussionSearch
-                    state={this.search}
-                    onSelect={((discussion: Discussion) => {
-                      this.reasonDetail = Stream(discussion.id());
-                    }).bind(this)}
-                    ignore={discussion.id()}
-                  />
-                ) : (
-                  ''
-                )}
-              </label>
-            );
-          });
-        }
-      });
+      if (this.attrs.post?.number() === 1) {
+        findFirstVdomChild(vnode, '.Form-group', (vnode) => {
+          const discussion = this.attrs.post.discussion();
+          this.search = new GlobalSearchState();
+
+          vnode.children[0].children.splice(
+            0,
+            0,
+            <label className="checkbox">
+              <input type="radio" name="reason" checked={this.reason() === 'duplicate'} value="duplicate" onclick={withAttr('value', this.reason)} />
+              <strong>{app.translator.trans('blomstra-flag-duplicates.forum.flags.reason_duplicate_label')}</strong>
+              {app.translator.trans('blomstra-flag-duplicates.forum.flags.reason_duplicate_text')}
+              {this.reason() === 'duplicate' ? (
+                <components.DiscussionSearch
+                  state={this.search}
+                  onSelect={((discussion: Discussion) => {
+                    this.reasonDetail = Stream(discussion.id());
+                  }).bind(this)}
+                  ignore={discussion.id()}
+                />
+              ) : (
+                ''
+              )}
+            </label>
+          );
+        });
+      }
+    });
   }
 
   // Requires https://github.com/flarum/flags/pull/39
@@ -75,17 +79,46 @@ export default function extendFlagModal() {
     }
   });
 
+  extend(Post.prototype, 'flagActionItems', function (items: ItemList) {
+    const flags = this.attrs.post?.flags?.();
+
+    // If (at least) one of the flags here is `duplicate`, then we'll remove the `Delete` button and replace it with a `Merge` button.
+    flags.map((flag: Flag) => {
+      if (flag?.reason?.() === 'duplicate') {
+        const discussion = this.attrs.post.discussion();
+        
+        items.remove('controls');
+        items.add(
+          'merge',
+          <Button className="Button" icon="fas fa-code-branch fa-flip-vertical" disabled={!!!this.dupeDiscussion} onclick={() => {app.modal.show(components.DiscussionMergeModal, { discussion, preselect: this.dupeDiscussion })}}>
+            {app.translator.trans('fof-merge-discussions.forum.discussion.merge')}
+          </Button>,
+          5
+        );
+      }
+    });
+  });
+
   override(Post.prototype, 'flagReason', function (original, flag: Flag) {
     const orig = original(flag);
 
     if (flag?.reason?.() === 'duplicate') {
-      const detail = flag.reasonDetail();
-      const url = app.forum.attribute('baseUrl') + `/d/${detail}`;
+      if (this.flagsLoading || !this.dupeDiscussion) {
+        orig[1] = (
+          <span className='Post-flagged-detail'>
+            <LoadingIndicator size='small' display='inline'/>
+          </span>
+        );
+
+        return orig;
+      }
+
+      const url = app.route.discussion(this.dupeDiscussion);
 
       orig[1] = (
         <span className="Post-flagged-detail">
           <Link href={url} external={true} target="_blank">
-            {app.translator.trans('blomstra-flag-duplicates.forum.flags.duplicate_discussion')} {icon('fas fa-external-link-alt')}{' '}
+            {this.dupeDiscussion.title() || app.translator.trans('blomstra-flag-duplicates.forum.flags.duplicate_discussion')} {icon('fas fa-external-link-alt')}{' '}
           </Link>
         </span>
       );
@@ -94,4 +127,20 @@ export default function extendFlagModal() {
       return orig;
     }
   });
+
+  extend(Post.prototype, 'oninit', function(this: Post, vnode: Mithril.Vnode) {
+    const flags = this.attrs.post?.flags?.();
+    
+    if (flags) {
+      this.flagsLoading = true;
+      flags.map((flag: Flag) => {
+        if (flag?.reason?.() === 'duplicate') {
+          app.store.find('discussions', flag.reasonDetail()).then((discussion: Discussion) => {
+            this.dupeDiscussion = discussion;
+          });
+        }
+      });
+      this.flagsLoading = false;
+    }
+  })
 }
