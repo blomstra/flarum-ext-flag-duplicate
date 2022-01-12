e2fd73705907afe1277d0c1dcf5f18e4a48fbfbf
Ian Morland <ian@morland.me>
2022-01-12 15:52:44 +0000

    Add listener, auto dismiss duplicate flags after merge

---
+++ src/Listener/RemoveDuplicateFlagAfterMerge.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Blomstra\FlagDuplicates\Listener;
+
+use Flarum\Flags\Event\Deleting;
+use Flarum\Flags\Flag;
+use FoF\MergeDiscussions\Events\DiscussionWasMerged;
+use Illuminate\Contracts\Events\Dispatcher;
+use Illuminate\Database\Eloquent\Collection;
+
+class RemoveDuplicateFlagAfterMerge
+{
+    /** @var Dispatcher $events */
+    protected $events;
+    
+    public function __construct(Dispatcher $events)
+    {
+        $this->events = $events;
+    }
+    
+    public function handle(DiscussionWasMerged $event)
+    {
+        $discussion = $event->discussion;
+
+        /** @var array $postIds */
+        $postIds = $discussion->posts->pluck('id');
+
+        /** @var Collection $flags */
+        $flags = Flag::where('reason', 'duplicate')->whereIn('post_id', $postIds)->where('reason_detail', $discussion->id)->get();
+
+        foreach ($flags as $flag) {
+            /** @var Flag $flag */
+            $this->events->dispatch(new Deleting($flag, $event->actor));
+            $flag->delete();
+        }
+
+        $discussion->refresh();
+    }
+}
